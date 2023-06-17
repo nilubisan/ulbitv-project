2b5cf093646353f8a69f31cda4c9ee5226f1257e
Rinat N <rinat-n@auto.kz>
2023-06-17 23:09:56 +0600

    dev: add config

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,35 @@
+import {RuleSetRule} from 'webpack'
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
+export function buildLoaders(options: BuildOptions): RuleSetRule[] {
+    const { isDev } = options
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        auto: (resPath: string) => !!resPath.includes('.module.'),
+                        localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]',
+                    }
+                },
+            },
+            "sass-loader",
+        ]
+    }
+
+    const tsLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+    }
+
+    return [
+        tsLoader,
+        cssLoader
+    ]
+}
---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,30 @@
+import { Configuration } from 'webpack';
+import {BuildOptions} from "./types/config";
+import path from "path";
+import {buildPlugins} from "./buildPlugins";
+import {buildLoaders} from "./buildLoaders";
+import {buildResolvers} from "./buildResolvers";
+import {buildDevServer} from "./buildDevServer";
+
+
+export function buildWebpackConfig(options: BuildOptions): Configuration {
+    const {paths, mode, isDev} = options
+
+    return {
+        mode: mode,
+        entry: paths.entry,
+            output: {
+        filename: '[name].[contenthash].js',
+            path: paths.build,
+            clean: true
+    },
+        plugins: buildPlugins(options),
+
+        module: {
+            rules: buildLoaders(options),
+        },
+        resolve: buildResolvers(),
+        devtool: isDev && 'inline-source-map',
+        devServer: isDev ? buildDevServer(options) : {}
+    }
+}
