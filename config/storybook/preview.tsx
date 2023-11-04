5385e3c59babb6fd6ca3ae03c52fa5d70f1d8bc6
Rinat N <rinat-n@auto.kz>
2023-11-04 22:24:58 +0600

    dev: add store provider for storybook

--- config/storybook/preview.tsx
+++ config/storybook/preview.tsx
@@ -3,6 +3,7 @@ import StorybookWrapper from 'app/StorybookWrapper'
 import { Theme, ThemeProvider } from 'app/providers/ThemeProvider'
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator'
 import { RouterDecorator } from 'shared/config/storybook/RouterDecorator/RouterDecorator'
+import { StoreProvider } from 'app/providers/StoreProvider'
 
 const preview: Preview = {
   parameters: {
@@ -18,11 +19,13 @@ const preview: Preview = {
     ThemeDecorator(Theme.LIGHT),
     StoryFn => {
       return (
+          <StoreProvider>
             <ThemeProvider>
               <StorybookWrapper>
                 <StoryFn/>
               </StorybookWrapper>
             </ThemeProvider>
+          </StoreProvider>
       )
     },
     RouterDecorator
--- src/pages/Main/ui/Main.tsx
+++ src/pages/Main/ui/Main.tsx
@@ -6,7 +6,7 @@ const Main = () => {
   return (
         <div>
             {t('Главная страница')}
-            {/*<Counter />*/}
+            <Counter />
         </div>
   )
 }
