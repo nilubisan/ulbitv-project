a7739a9206cd224e05446ba4b6e90b27f4fc6dc6
Rinat N <rinat-n@auto.kz>
2023-11-05 16:33:21 +0600

    dev: fix storybook webpack config

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import type webpack from 'webpack'
+import webpack from 'webpack'
 import { type BuildPaths } from '../build/types/config'
 import path from 'path'
 import { buildCssLoaders } from '../build/loaders/buildCssLoaders'
@@ -13,6 +13,11 @@ export default ({ config }: { config: webpack.Configuration }) => {
   }
   config.resolve.modules.push(paths.src)
   config.resolve.extensions.push('.ts', '.tsx')
+  config.resolve.plugins.push(
+    new webpack.DefinePlugin({
+      __IS_DEV__: true
+    })
+  )
 
   config.module.rules = config.module.rules.map((rule) => {
     if (/svg/.test(rule.test)) {
